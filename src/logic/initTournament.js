f50bb7870ccbddb2bd41732ed1ece518758ec35f
Federico Pi Dote <fpidote@hotmail.com>
2026-06-02 17:39:24 +0200

    feat: agregar flag pozoMode al config inicial de El Pozo

--- src/logic/initTournament.js
+++ src/logic/initTournament.js
@@ -59,6 +59,7 @@ export function buildInitialTournament(type, ownerUid) {
     },
     pozo: {
       ...base,
+      config: { ...base.config, pozoMode: "fixed" },
       pairInputs: [],
       pozoRounds: [],
       currentPozoRound: null,
